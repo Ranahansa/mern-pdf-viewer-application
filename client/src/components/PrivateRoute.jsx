2ec5dbfacbf865d3b04f07d53e260f170d641ca9
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-06-11 19:55:43 +0530

    Update PrivateRoute component to redirect to "/upload" when user is not authenticated

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const PrivateRoute = () => {
     const isAuthenticated = !!localStorage.getItem('token');
 
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+    return isAuthenticated ? <Outlet /> : <Navigate to="/upload" />;
 };
 
 export default PrivateRoute;
