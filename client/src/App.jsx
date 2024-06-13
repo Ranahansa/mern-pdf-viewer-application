3eead2382dc7857de39bec43be0a15288919020d
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-06-13 12:07:05 +0530

    Add logout functionality and update navbar based on user login status

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import UploadPdf from './components/UploadPdf';
@@ -10,14 +10,27 @@ import Home from './components/Home';
 import Footer from './components/Footer';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsLoggedIn(!!token);
+  }, []);
+  
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
+    navigate('/');
+  }
   return (
     <Router>
       <div className="container px-4 mx-auto">
-        <Navbar />
+        <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
           <Route element={<PrivateRoute />}>
             <Route path="/upload" element={<UploadPdf />} />
             <Route path="/view" element={<ViewPdfs />} />
